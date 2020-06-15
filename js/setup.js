49a25ac2d123c7fb56dec5d958b9c75760a3e297
Наташа <antonenko_natash@mail.ru>
2020-06-15 21:21:18 +0300

    Добавляет исправления

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,6 @@
 'use strict';
 
+window.WIZARDS = [];
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -125,20 +126,20 @@ var getRandomIndex = function (array) {
 var getRandomName = function (wizardIndex) {
   var indexName = getRandomIndex(WIZARD_NAMES);
   var indexSurname = getRandomIndex(WIZARD_SURNAMES);
-  Wizards[wizardIndex].name = WIZARD_NAMES[indexName] + ' ' + WIZARD_SURNAMES[indexSurname];
-  return Wizards[wizardIndex].name;
+  window.WIZARDS[wizardIndex].name = WIZARD_NAMES[indexName] + ' ' + WIZARD_SURNAMES[indexSurname];
+  return window.WIZARDS[wizardIndex].name;
 };
 
 var getRandomCoatColor = function (wizardIndex) {
   var indexCoatColor = getRandomIndex(WIZARD_COAT_COLOR);
-  Wizards[wizardIndex].coatColor = WIZARD_COAT_COLOR[indexCoatColor];
-  return Wizards[wizardIndex].coatColor;
+  window.WIZARDS[wizardIndex].coatColor = WIZARD_COAT_COLOR[indexCoatColor];
+  return window.WIZARDS[wizardIndex].coatColor;
 };
 
 var getRandomEyesColor = function (wizardIndex) {
   var indexEyesColor = getRandomIndex(WIZARD_EYES_COLOR);
-  Wizards[wizardIndex].eyesColor = WIZARD_EYES_COLOR[indexEyesColor];
-  return Wizards[wizardIndex].eyesColor;
+  window.WIZARDS[wizardIndex].eyesColor = WIZARD_EYES_COLOR[indexEyesColor];
+  return window.WIZARDS[wizardIndex].eyesColor;
 };
 
 var renderWizard = function (wizard) {
@@ -153,8 +154,7 @@ var renderWizard = function (wizard) {
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < WIZARDS_COUNT; i++) {
-  var Wizards = [];
-  Wizards[i] = {};
+  window.WIZARDS[i] = {};
   fragment.appendChild(renderWizard(i));
 }
 similarListElement.appendChild(fragment);
